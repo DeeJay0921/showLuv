4272e5328b703879af984cc9d26687cb4d7b19c3
DeeJay0921 <1018805743@qq.com>
2018-06-19 23:10:12 +0800

    add HeartClick

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <div id="app">
+    <HeartClick></HeartClick>
   </div>
 </template>
 
 <script>
+  import HeartClick from './components/heartClick'
 
-export default {
-  name: 'App',
-  components: {
-
+  export default {
+    name: 'App',
+    components: {
+      HeartClick
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 100%;
-  background: #ccc;
-}
+  html,body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100%;
+  }
 </style>
---
+++ src/components/heartClick.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="heart_click" @click=animate>
+        <div id="heart">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeartClick',
+    methods: {
+        animate() {
+
+        }
+    }
+}
+</script>
+
+<style>
+    #heart_click {
+        width:  100%;
+        height:  100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    #heart {
+        width: 15vw;
+        height: 15vw;
+        background-color: crimson;
+        transform: rotate(45deg);
+        position: relative;
+    }
+    #heart::after,#heart::before {
+        position: absolute;
+        content: '';
+        display: block;
+        width: 15vw;
+        height: 15vw;
+        background-color: crimson;
+        border-radius: 50%;
+    }
+    #heart::before {
+        left: -7.5vw;
+        top: 0;
+    }
+    #heart::after {
+        right: 0;
+        top: -7.5vw;
+    }
+</style>
